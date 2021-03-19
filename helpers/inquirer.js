24da99f616083e3f1546293198f1e34b7864a06f
jela3105 <jela3105@gmail.com>
2021-03-18 22:25:20 -0600

    Add options in menu

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,7 +5,15 @@ const questions = [
     type: "list",
     name: "option",
     message: "What would you like to do?",
-    choices: ["opt1", "opt2", "opt3"],
+    choices: [
+      { value: "1", name: "1. Create task" },
+      { value: "2", name: "2. List tasks" },
+      { value: "3", name: "3. List tasks completed" },
+      { value: "4", name: "4. List tasks uncompleted" },
+      { value: "5", name: "5. Complete task(s)" },
+      { value: "6", name: "6. Delete task" },
+      { value: "0", name: "0. Exit" },
+    ],
   },
 ];
 
@@ -14,8 +22,8 @@ const inquirerMenu = async () => {
   console.log("============================================".green);
   console.log("	  Selected some option     ");
   console.log("============================================\n".green);
-  const opt = await inquirer.prompt(questions);
-  return opt;
+  const { option } = await inquirer.prompt(questions);
+  return option;
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("colors");
 
 const { inquirerMenu } = require("./helpers/inquirer");
+const { pause } = require("./helpers/messages");
 
 const main = async () => {
   console.clear();
   let opt = "";
   do {
     opt = await inquirerMenu();
+    console.log({ opt });
     if (opt !== "0") await pause();
   } while (opt !== "0");
 };
